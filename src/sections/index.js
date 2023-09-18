c480b9c2e5404bd7bf72de62d70daac71ff8b456
Edwin Festus <edwinfestusdev@gmail.com>
2023-09-18 02:27:35 +0100

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import Hero from './Hero'
+import PopularProducts from './PopularProducts'
+import SpecialOffer from './SpecialOffer'
+import Services from './Services'
+import CustomerReview from './CustomerReviews'
+import Subscribe from './Subscribe'
+import SuperQuality from './SuperQuality'
+import Footer from './Footer'
+
+
+export {
+    Hero,
+    PopularProducts,
+    SpecialOffer,
+    Services,
+    CustomerReview,
+    Subscribe,
+    SuperQuality,
+    Footer
+}
